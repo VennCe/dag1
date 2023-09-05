dd94aa290e9ec01b77cc18018381281aca81820e
VennCe <savannah.c.esajas@hotmail.com>
2023-09-05 16:22:23 +0200

    aanpassingen

--- simple.py
+++ simple.py
@@ -1,8 +1,13 @@
 #comentaar
 
 print ("hello world!")
+print ("Dit is voor git")
 
-def function1():
+def funct1():
     print ('Test')
 
-funct1()
+def funct2(tekst):
+    print ('Ik print 1234 hier' + tekst)
+
+    funct1 ()
+    funct2 ("mijn tekst")
